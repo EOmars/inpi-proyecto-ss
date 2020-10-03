e4d273147a1a164139bcc99c73808a827a2f2006
elias <pakatsy@ciencias.unam.mx>
2020-10-03 16:53:57 -0500

    imagenes reducidas y traduccion agregada

--- js/proyecto.js
+++ js/proyecto.js
@@ -8,13 +8,51 @@ const escorpion = document.getElementById('escorpion')
 const tortuga = document.getElementById('tortuga')
 const vibora = document.getElementById('vibora')
 
-sol.onclick = (e) => document.getElementById('sol-audio').play()
-arbol.onclick = (e) => document.getElementById('arbol-audio').play()
-lagartija.onclick = (e) => document.getElementById('lagartija-audio').play()
-vaca.onclick = (e) => document.getElementById('vaca-audio').play()
-pez.onclick = (e) => document.getElementById('pez-audio').play()
-cangrejo.onclick = (e) => document.getElementById('cangrejo-audio').play()
-escorpion.onclick = (e) => document.getElementById('escorpion-audio').play()
-tortuga.onclick = (e) => document.getElementById('tortuga-audio').play()
-vibora.onclick = (e) => document.getElementById('vibora-audio').play()
+sol.onclick = (e) => {
+    document.getElementById('sol-audio').play()
+    document.getElementById('sol-texto').classList.add('show')
+}
+
+arbol.onclick = (e) => {
+    document.getElementById('arbol-audio').play()
+    document.getElementById('arbol-texto').classList.add('show')
+}
+
+lagartija.onclick = (e) => {
+    document.getElementById('lagartija-audio').play()
+    document.getElementById('lagartija-texto').classList.add('show')
+}
+
+vaca.onclick = (e) => {
+    document.getElementById('vaca-audio').play()
+    document.getElementById('vaca-texto').classList.add('show')
+}
+
+pez.onclick = (e) => {
+    document.getElementById('pez-texto').classList.add('show')
+    document.getElementById('pez-audio').play()    
+}
+
+cangrejo.onclick = (e) => {
+    document.getElementById('cangrejo-audio').play()
+    document.getElementById('cangrejo-texto').classList.add('show')
+}
+
+escorpion.onclick = (e) => {
+    document.getElementById('escorpion-audio').play()
+    document.getElementById('escorpion-texto').classList.add('show')
+}
+tortuga.onclick = (e) => {
+    document.getElementById('tortuga-audio').play()
+    document.getElementById('tortuga-texto').classList.add('show')
+}
+
+vibora.onclick = (e) => {
+    document.getElementById('vibora-audio').play()
+    document.getElementById('vibora-texto').classList.add('show')
+}
+
+function ocultarPopup(e) {
+    e.children[0].classList.toggle('show')
+}
 
