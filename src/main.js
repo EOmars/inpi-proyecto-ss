c86ded31cdbfc54a901e0fd6618876f7067422e6
elias <pakatsy@ciencias.unam.mx>
2020-10-30 10:36:20 -0600

    agregado scale

--- src/main.js
+++ src/main.js
@@ -10,13 +10,8 @@ export default new Phaser.Game({
     width: 1280,
     height: 720,
     scene: Scene1,
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: {
-                y: 200
-            },
-            debug: true
-        }
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
     }
 })
